30719a88f3900c9f7661b6b031d668ac785237fa
Carlisia <carlisiac@vmware.com>
2019-04-02 11:26:43 -0700

    Update vendoring
    Signed-off-by: Carlisia <carlisiac@vmware.com>

--- vendor/github.com/heptio/velero/pkg/plugin/generated/RestoreItemAction.pb.go
+++ vendor/github.com/heptio/velero/pkg/plugin/generated/RestoreItemAction.pb.go
@@ -17,65 +17,75 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-type RestoreExecuteRequest struct {
-	Plugin  string `protobuf:"bytes,1,opt,name=plugin" json:"plugin,omitempty"`
-	Item    []byte `protobuf:"bytes,2,opt,name=item,proto3" json:"item,omitempty"`
-	Restore []byte `protobuf:"bytes,3,opt,name=restore,proto3" json:"restore,omitempty"`
+type RestoreItemActionExecuteRequest struct {
+	Plugin         string `protobuf:"bytes,1,opt,name=plugin" json:"plugin,omitempty"`
+	Item           []byte `protobuf:"bytes,2,opt,name=item,proto3" json:"item,omitempty"`
+	Restore        []byte `protobuf:"bytes,3,opt,name=restore,proto3" json:"restore,omitempty"`
+	ItemFromBackup []byte `protobuf:"bytes,4,opt,name=itemFromBackup,proto3" json:"itemFromBackup,omitempty"`
 }
 
-func (m *RestoreExecuteRequest) Reset()                    { *m = RestoreExecuteRequest{} }
-func (m *RestoreExecuteRequest) String() string            { return proto.CompactTextString(m) }
-func (*RestoreExecuteRequest) ProtoMessage()               {}
-func (*RestoreExecuteRequest) Descriptor() ([]byte, []int) { return fileDescriptor4, []int{0} }
+func (m *RestoreItemActionExecuteRequest) Reset()                    { *m = RestoreItemActionExecuteRequest{} }
+func (m *RestoreItemActionExecuteRequest) String() string            { return proto.CompactTextString(m) }
+func (*RestoreItemActionExecuteRequest) ProtoMessage()               {}
+func (*RestoreItemActionExecuteRequest) Descriptor() ([]byte, []int) { return fileDescriptor3, []int{0} }
 
-func (m *RestoreExecuteRequest) GetPlugin() string {
+func (m *RestoreItemActionExecuteRequest) GetPlugin() string {
 	if m != nil {
 		return m.Plugin
 	}
 	return ""
 }
 
-func (m *RestoreExecuteRequest) GetItem() []byte {
+func (m *RestoreItemActionExecuteRequest) GetItem() []byte {
 	if m != nil {
 		return m.Item
 	}
 	return nil
 }
 
-func (m *RestoreExecuteRequest) GetRestore() []byte {
+func (m *RestoreItemActionExecuteRequest) GetRestore() []byte {
 	if m != nil {
 		return m.Restore
 	}
 	return nil
 }
 
-type RestoreExecuteResponse struct {
-	Item    []byte `protobuf:"bytes,1,opt,name=item,proto3" json:"item,omitempty"`
-	Warning string `protobuf:"bytes,2,opt,name=warning" json:"warning,omitempty"`
+func (m *RestoreItemActionExecuteRequest) GetItemFromBackup() []byte {
+	if m != nil {
+		return m.ItemFromBackup
+	}
+	return nil
 }
 
-func (m *RestoreExecuteResponse) Reset()                    { *m = RestoreExecuteResponse{} }
-func (m *RestoreExecuteResponse) String() string            { return proto.CompactTextString(m) }
-func (*RestoreExecuteResponse) ProtoMessage()               {}
-func (*RestoreExecuteResponse) Descriptor() ([]byte, []int) { return fileDescriptor4, []int{1} }
+type RestoreItemActionExecuteResponse struct {
+	Item            []byte                `protobuf:"bytes,1,opt,name=item,proto3" json:"item,omitempty"`
+	AdditionalItems []*ResourceIdentifier `protobuf:"bytes,2,rep,name=additionalItems" json:"additionalItems,omitempty"`
+}
 
-func (m *RestoreExecuteResponse) GetItem() []byte {
+func (m *RestoreItemActionExecuteResponse) Reset()         { *m = RestoreItemActionExecuteResponse{} }
+func (m *RestoreItemActionExecuteResponse) String() string { return proto.CompactTextString(m) }
+func (*RestoreItemActionExecuteResponse) ProtoMessage()    {}
+func (*RestoreItemActionExecuteResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor3, []int{1}
+}
+
+func (m *RestoreItemActionExecuteResponse) GetItem() []byte {
 	if m != nil {
 		return m.Item
 	}
 	return nil
 }
 
-func (m *RestoreExecuteResponse) GetWarning() string {
+func (m *RestoreItemActionExecuteResponse) GetAdditionalItems() []*ResourceIdentifier {
 	if m != nil {
-		return m.Warning
+		return m.AdditionalItems
 	}
-	return ""
+	return nil
 }
 
 func init() {
-	proto.RegisterType((*RestoreExecuteRequest)(nil), "generated.RestoreExecuteRequest")
-	proto.RegisterType((*RestoreExecuteResponse)(nil), "generated.RestoreExecuteResponse")
+	proto.RegisterType((*RestoreItemActionExecuteRequest)(nil), "generated.RestoreItemActionExecuteRequest")
+	proto.RegisterType((*RestoreItemActionExecuteResponse)(nil), "generated.RestoreItemActionExecuteResponse")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -90,7 +100,7 @@ const _ = grpc.SupportPackageIsVersion4
 
 type RestoreItemActionClient interface {
 	AppliesTo(ctx context.Context, in *AppliesToRequest, opts ...grpc.CallOption) (*AppliesToResponse, error)
-	Execute(ctx context.Context, in *RestoreExecuteRequest, opts ...grpc.CallOption) (*RestoreExecuteResponse, error)
+	Execute(ctx context.Context, in *RestoreItemActionExecuteRequest, opts ...grpc.CallOption) (*RestoreItemActionExecuteResponse, error)
 }
 
 type restoreItemActionClient struct {
@@ -110,8 +120,8 @@ func (c *restoreItemActionClient) AppliesTo(ctx context.Context, in *AppliesToRe
 	return out, nil
 }
 
-func (c *restoreItemActionClient) Execute(ctx context.Context, in *RestoreExecuteRequest, opts ...grpc.CallOption) (*RestoreExecuteResponse, error) {
-	out := new(RestoreExecuteResponse)
+func (c *restoreItemActionClient) Execute(ctx context.Context, in *RestoreItemActionExecuteRequest, opts ...grpc.CallOption) (*RestoreItemActionExecuteResponse, error) {
+	out := new(RestoreItemActionExecuteResponse)
 	err := grpc.Invoke(ctx, "/generated.RestoreItemAction/Execute", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
@@ -123,7 +133,7 @@ func (c *restoreItemActionClient) Execute(ctx context.Context, in *RestoreExecut
 
 type RestoreItemActionServer interface {
 	AppliesTo(context.Context, *AppliesToRequest) (*AppliesToResponse, error)
-	Execute(context.Context, *RestoreExecuteRequest) (*RestoreExecuteResponse, error)
+	Execute(context.Context, *RestoreItemActionExecuteRequest) (*RestoreItemActionExecuteResponse, error)
 }
 
 func RegisterRestoreItemActionServer(s *grpc.Server, srv RestoreItemActionServer) {
@@ -149,7 +159,7 @@ func _RestoreItemAction_AppliesTo_Handler(srv interface{}, ctx context.Context, 
 }
 
 func _RestoreItemAction_Execute_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RestoreExecuteRequest)
+	in := new(RestoreItemActionExecuteRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -161,7 +171,7 @@ func _RestoreItemAction_Execute_Handler(srv interface{}, ctx context.Context, de
 		FullMethod: "/generated.RestoreItemAction/Execute",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(RestoreItemActionServer).Execute(ctx, req.(*RestoreExecuteRequest))
+		return srv.(RestoreItemActionServer).Execute(ctx, req.(*RestoreItemActionExecuteRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -183,23 +193,26 @@ var _RestoreItemAction_serviceDesc = grpc.ServiceDesc{
 	Metadata: "RestoreItemAction.proto",
 }
 
-func init() { proto.RegisterFile("RestoreItemAction.proto", fileDescriptor4) }
+func init() { proto.RegisterFile("RestoreItemAction.proto", fileDescriptor3) }
 
-var fileDescriptor4 = []byte{
-	// 230 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x0f, 0x4a, 0x2d, 0x2e,
-	0xc9, 0x2f, 0x4a, 0xf5, 0x2c, 0x49, 0xcd, 0x75, 0x4c, 0x2e, 0xc9, 0xcc, 0xcf, 0xd3, 0x2b, 0x28,
-	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x4c, 0x4f, 0xcd, 0x4b, 0x2d, 0x4a, 0x2c, 0x49, 0x4d, 0x91, 0xe2,
-	0x09, 0xce, 0x48, 0x2c, 0x4a, 0x4d, 0x81, 0x48, 0x28, 0xc5, 0x72, 0x89, 0x42, 0xf5, 0xb8, 0x56,
-	0xa4, 0x26, 0x97, 0x96, 0xa4, 0x06, 0xa5, 0x16, 0x96, 0xa6, 0x16, 0x97, 0x08, 0x89, 0x71, 0xb1,
-	0x15, 0xe4, 0x94, 0xa6, 0x67, 0xe6, 0x49, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x41, 0x79, 0x42,
-	0x42, 0x5c, 0x2c, 0x99, 0x25, 0xa9, 0xb9, 0x12, 0x4c, 0x0a, 0x8c, 0x1a, 0x3c, 0x41, 0x60, 0xb6,
-	0x90, 0x04, 0x17, 0x7b, 0x11, 0xc4, 0x10, 0x09, 0x66, 0xb0, 0x30, 0x8c, 0xab, 0xe4, 0xc6, 0x25,
-	0x86, 0x6e, 0x7c, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0x2a, 0xdc, 0x1c, 0x46, 0x54, 0x73, 0xca, 0x13,
-	0x8b, 0xf2, 0x32, 0xf3, 0xd2, 0xc1, 0xc6, 0x73, 0x06, 0xc1, 0xb8, 0x46, 0xab, 0x19, 0xb9, 0x04,
-	0x31, 0xfc, 0x26, 0xe4, 0xc6, 0xc5, 0xe9, 0x58, 0x50, 0x90, 0x93, 0x99, 0x5a, 0x1c, 0x92, 0x2f,
-	0x24, 0xad, 0x07, 0xf7, 0xa3, 0x1e, 0x5c, 0x14, 0xea, 0x1b, 0x29, 0x19, 0xec, 0x92, 0x50, 0xb7,
-	0xf8, 0x71, 0xb1, 0x43, 0x9d, 0x27, 0xa4, 0x80, 0xa4, 0x10, 0x6b, 0xc0, 0x48, 0x29, 0xe2, 0x51,
-	0x01, 0x31, 0x2f, 0x89, 0x0d, 0x1c, 0xb6, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xaa, 0xe5,
-	0x97, 0xa4, 0x8f, 0x01, 0x00, 0x00,
+var fileDescriptor3 = []byte{
+	// 280 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x51, 0xdb, 0x4a, 0x03, 0x31,
+	0x10, 0x25, 0x6d, 0x69, 0xe9, 0x58, 0x14, 0xf3, 0xa0, 0x61, 0x55, 0x5c, 0xfa, 0x20, 0x8b, 0xc2,
+	0x3e, 0xd4, 0x2f, 0xa8, 0x60, 0xa5, 0xaf, 0xd1, 0x1f, 0xd8, 0x6e, 0xc6, 0x1a, 0xdc, 0x4d, 0x62,
+	0x2e, 0xe0, 0x9b, 0x9f, 0xe0, 0x27, 0xf9, 0x6b, 0xb2, 0x17, 0x97, 0xda, 0xaa, 0xf8, 0x96, 0x39,
+	0x73, 0xce, 0x9c, 0x33, 0x19, 0x38, 0xe6, 0xe8, 0xbc, 0xb6, 0xb8, 0xf4, 0x58, 0xce, 0x73, 0x2f,
+	0xb5, 0x4a, 0x8d, 0xd5, 0x5e, 0xd3, 0xf1, 0x1a, 0x15, 0xda, 0xcc, 0xa3, 0x88, 0x26, 0xf7, 0x4f,
+	0x99, 0x45, 0xd1, 0x34, 0xa6, 0xef, 0x04, 0xce, 0x77, 0x44, 0xb7, 0xaf, 0x98, 0x07, 0x8f, 0x1c,
+	0x5f, 0x02, 0x3a, 0x4f, 0x8f, 0x60, 0x68, 0x8a, 0xb0, 0x96, 0x8a, 0x91, 0x98, 0x24, 0x63, 0xde,
+	0x56, 0x94, 0xc2, 0x40, 0x7a, 0x2c, 0x59, 0x2f, 0x26, 0xc9, 0x84, 0xd7, 0x6f, 0xca, 0x60, 0x64,
+	0x9b, 0x71, 0xac, 0x5f, 0xc3, 0x5f, 0x25, 0xbd, 0x80, 0xfd, 0x8a, 0xb1, 0xb0, 0xba, 0xbc, 0xc9,
+	0xf2, 0xe7, 0x60, 0xd8, 0xa0, 0x26, 0x6c, 0xa1, 0xd3, 0x37, 0x88, 0x7f, 0x0f, 0xe4, 0x8c, 0x56,
+	0x0e, 0x3b, 0x67, 0xb2, 0xe1, 0x7c, 0x07, 0x07, 0x99, 0x10, 0xb2, 0xa2, 0x67, 0x45, 0x25, 0x75,
+	0xac, 0x17, 0xf7, 0x93, 0xbd, 0xd9, 0x59, 0xda, 0x2d, 0x9f, 0x72, 0x74, 0x3a, 0xd8, 0x1c, 0x97,
+	0x02, 0x95, 0x97, 0x8f, 0x12, 0x2d, 0xdf, 0x56, 0xcd, 0x3e, 0x08, 0x1c, 0xee, 0x24, 0xa0, 0x0b,
+	0x18, 0xcf, 0x8d, 0x29, 0x24, 0xba, 0x07, 0x4d, 0x4f, 0x36, 0x46, 0x76, 0x68, 0xfb, 0x5d, 0xd1,
+	0xe9, 0xcf, 0xcd, 0x36, 0xfa, 0x0a, 0x46, 0xed, 0x36, 0xf4, 0xf2, 0x7b, 0xb0, 0xbf, 0x6e, 0x10,
+	0x5d, 0xfd, 0x8b, 0xdb, 0x78, 0xac, 0x86, 0xf5, 0x6d, 0xaf, 0x3f, 0x03, 0x00, 0x00, 0xff, 0xff,
+	0xef, 0xe4, 0xd1, 0x78, 0x0f, 0x02, 0x00, 0x00,
 }
