4d1290e51d0f878098bde46befe031e1a9fc6714
KubeKween <carlisia@grokkingtech.io>
2019-04-03 06:29:12 -0700

    Update examples for 1.0 (#21)
    * Update vendoring
    
    Signed-off-by: Carlisia <carlisiac@vmware.com>
    
    * update examples for 1.0
    
    Signed-off-by: Carlisia <carlisiac@vmware.com>
    
    * Make name prefx a fqdn
    
    Signed-off-by: Carlisia <carlisiac@vmware.com>

---
+++ velero-examples/backupplugin.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2017, 2019 the Velero contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	v1 "github.com/heptio/velero/pkg/apis/velero/v1"
+	"github.com/heptio/velero/pkg/plugin/velero"
+)
+
+// BackupPlugin is a backup item action plugin for Velero.
+type BackupPlugin struct {
+	log logrus.FieldLogger
+}
+
+// AppliesTo returns information about which resources this action should be invoked for.
+// A BackupPlugin's Execute function will only be invoked on items that match the returned
+// selector. A zero-valued ResourceSelector matches all resources.
+func (p *BackupPlugin) AppliesTo() (velero.ResourceSelector, error) {
+	return velero.ResourceSelector{}, nil
+}
+
+// Execute allows the ItemAction to perform arbitrary logic with the item being backed up,
+// in this case, setting a custom annotation on the item being backed up.
+func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (runtime.Unstructured, []velero.ResourceIdentifier, error) {
+	p.log.Info("Hello from my BackupPlugin!")
+
+	metadata, err := meta.Accessor(item)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	annotations := metadata.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	annotations["velero.io/my-backup-plugin"] = "1"
+
+	metadata.SetAnnotations(annotations)
+
+	return item, nil, nil
+}
